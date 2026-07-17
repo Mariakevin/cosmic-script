485682c42cd99f973e5d8832752d8c47f46f6aee
Mariakevin <mariakevin0504@gmail.com>
2026-07-17 17:55:27 +0530

    chore: cleanup project directory — remove temp files, update .gitignore

--- clean_cache.js
+++
@@ -1,8 +0,0 @@
-const { spawn } = require('child_process');
-const path = require('path');
-
-const venvPython = path.join(__dirname, '.venv', 'Scripts', 'python.exe');
-const code = `import shutil, glob; [shutil.rmtree(d, True) for d in glob.glob('cosmic_script/**/__pycache__', recursive=True)]`;
-
-const proc = spawn(venvPython, ['-c', code], { cwd: __dirname, stdio: 'inherit' });
-proc.on('close', (code) => process.exit(code));
